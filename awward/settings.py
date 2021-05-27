b5fe6fda862f0397c5ee915a63ac8db8a551773d
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-27 11:04:57 +0300

    updated settings

--- awward/settings.py
+++ awward/settings.py
@@ -29,9 +29,9 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('awardss'),
-           'USER': config('natasha'),
-           'PASSWORD': config('natasha'),
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
            'PORT': '',
        }
@@ -120,8 +120,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awardss',
-        'USER':'postgres',
-        'PASSWORD':'Openup47',
+        'USER':'natasha',
+        'PASSWORD':'natasha',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
